dac80ac0c6cbb3a2a2089d0e30a3d3f941fb9191
Tamara <tamara.zapatagomez@gmail.com>
2024-09-11 21:49:16 -0300

    pestaña arriendo y venta

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const properties = [
+const properties4sale = [
     {
         name: "Cabaña Acogedora en la Montaña",
         location: "Valle Nevado, Chile",
@@ -66,12 +66,85 @@ const properties = [
     }
 ];
 
+const properties4rent = [
+    {
+        name: "Departamento Moderno en Providencia",
+        location: "Providencia, Santiago, Chile",
+        price: "$750.000",
+        description: "Un departamento moderno con vista a la ciudad, ubicado cerca de centros comerciales y transporte público.",
+        src: "./assets/img/providencia.jpg",
+        rooms: 2,
+        bath: 2,
+        smoke: false,
+        pets: false
+    },
+    {
+        name: "Casa Familiar con Jardín en La Reina",
+        location: "La Reina, Santiago, Chile",
+        price: "$1.200.000",
+        description: "Amplia casa familiar con gran jardín, ideal para familias que buscan tranquilidad en un barrio residencial.",
+        src: "./assets/img/la-reina.png",
+        rooms: 4,
+        bath: 3,
+        smoke: false,
+        pets: true
+    },
+    {
+        name: "Loft Estilo Industrial en Barrio Italia",
+        location: "Barrio Italia, Santiago, Chile",
+        price: "$650.000",
+        description: "Loft con un diseño industrial, perfecto para jóvenes profesionales, a pasos de cafés y tiendas de diseño.",
+        src: "./assets/img/barrio-italia.png",
+        rooms: 1,
+        bath: 1,
+        smoke: false,
+        pets: true
+    },
+    {
+        name: "Penthouse de Lujo en Las Condes",
+        location: "Las Condes, Santiago, Chile",
+        price: "UF 35",
+        description: "Penthouse de lujo con terraza privada y vistas panorámicas a la cordillera de Los Andes, ideal para ejecutivos.",
+        src: "./assets/img/las-condes.png",
+        rooms: 5,
+        bath: 4,
+        smoke: false,
+        pets: false
+    },
+    {
+        name: "Estudio Céntrico en Santiago Centro",
+        location: "Santiago Centro, Chile",
+        price: "$350.000",
+        description: "Estudio céntrico, perfecto para quienes buscan vivir cerca del corazón de la ciudad con todas las comodidades.",
+        src: "./assets/img/santiago-centro.png",
+        rooms: 1,
+        bath: 1,
+        smoke: true,
+        pets: false
+    },
+    {
+        name: "Casa Rústica en Pirque",
+        location: "Pirque, Santiago, Chile",
+        price: "$850.000",
+        description: "Casa rústica con amplios terrenos y zonas verdes, ideal para desconectarse de la ciudad.",
+        src: "./assets/img/pirque.png",
+        rooms: 3,
+        bath: 2,
+        smoke: true,
+        pets: true
+    }
+];
+
 
 const propertiesContainer = document.getElementById('properties-container');
 
+const propertiesContainerRent = document.getElementById('properties-container-rent');
+
+
+
 
 // Función para renderizar todas las propiedades en el DOM
-function renderProperties(properties) {
+function renderProperties(properties, propertiesContainer) {
     properties.forEach(property => {
         // Maneja datos bool de propiedades
         const petsClass = property.pets ? 'pets-allowed' : 'pets-not-allowed';
@@ -113,4 +186,16 @@ function renderProperties(properties) {
 }
 
 // Llamar a la función para renderizar las propiedades
-renderProperties(properties);
+
+if(window.location.pathname.includes('index.html')){
+    renderProperties(properties4sale.slice(0,3), propertiesContainer)
+    renderProperties(properties4rent.slice(0,3), propertiesContainerRent)
+}
+if(window.location.pathname.includes('propiedades_alquiler.html')){
+    renderProperties(properties4rent, propertiesContainerRent)
+}
+if(window.location.pathname.includes('propiedades_venta.html')){
+    renderProperties(properties4sale, propertiesContainer)
+}
+
+
